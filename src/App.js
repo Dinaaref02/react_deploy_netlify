71c5b0d03a275ea530e17b1b1d800f59551ae9f0
Dina Aref <dinaref02@gmail.com>
2024-08-27 14:07:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import EditPost from "./EditPost";
+import { Route, Routes } from 'react-router-dom';
+import { DataProvider } from "./context/DataContext";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="React JS Blog" />
+      <DataProvider>
+        <Nav />
+        <Routes>
+          <Route exact path="/" Component={Home} />
+          <Route exact path="/post" Component={NewPost} />
+          <Route path="/edit/:id" Component={EditPost} />
+          <Route path="/post/:id" Component={PostPage} />
+          <Route path="/about" Component={About} />
+          <Route path="*" Component={Missing} />
+        </Routes>
+      </DataProvider>
+      <Footer />
     </div>
   );
 }
